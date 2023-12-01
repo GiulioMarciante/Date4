1492833f7376bab27d26e29c3e166570fd372679
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-12-01 16:16:23 +0100

    Esercizio Date4

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+//Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+//aggiungi un anno
+//sottrai un mese
+//aggiungi 7 giorni
+//Stampa il risultato localizzata per l'Italia
+public class Main {
+    public static void main(String[] args) {
+
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        OffsetDateTime newDate;
+        newDate = date
+                .plusYears(1)
+                .minusMonths(1)
+                .plusDays(7);
+
+        String dateString = newDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.ITALY));
+
+        System.out.println(dateString);
+    }
+}
